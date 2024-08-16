16956728e30321ce90298b14ef61f872cb8b2397
Brian Rolek <49486754+BrianRolek@users.noreply.github.com>
2024-08-16 15:57:15 -0300

    finalize. update best models using prob of direction.

--- R/postprocess.R
+++ R/postprocess.R
@@ -40,9 +40,9 @@ MCMCsummary(m.site1[[1]]$samples, pars,
             round=2, pg0 = TRUE, 
             func = median, func_name = "median")
 
-ps1 <- MCMCpstr(m.site1[[7]]$samples, pars, type="chains")
+ps1 <- MCMCpstr(m.site1[[2]]$samples, pars, type="chains")
 # Interaction with time is significant using p<0.1 and p>0.9
-# use model m[[7]]
+# use model m[[2]]
 
 MCMCsummary(m.site2[[8]]$samples, pars, 
             HPD=TRUE,  hpd_prob = 0.80,
@@ -70,7 +70,7 @@ coeftab.s1 <- MCMCsummary(m.site1[[1]]$samples, pars,
 coeftab.s1 <- coeftab.s1[, c(8,3,4,5,7)]
 write.csv(coeftab.s1, 'docs\\coeftab_s1.csv')
 # Subset of global model
-coeftab.s1.r <- MCMCsummary(m.site1[[7]]$samples, pars, 
+coeftab.s1.r <- MCMCsummary(m.site1[[2]]$samples, pars, 
                             HPD=TRUE,  hpd_prob = 0.95,
                             round=2, pg0 = TRUE, 
                             func = median, func_name = "median")
@@ -119,11 +119,11 @@ l.ab2 <- l.ab3 <- array(NA, dim=c(length(yr), 4000),
 for (t in 1:length(yr)){
   l.ab2[t, ] <- ps1$mu + 
                 ps1$delta[1,]*yr.sc[t] + 
-                #ps1$delta[2,]*yr.sc[t]^2 +
-                ps1$delta[2,]*cos(2*pi*cycle.sc2[t]) + 
-                ps1$delta[3,]*sin(2*pi*cycle.sc2[t]) +
-                ps1$delta[4,]*cos(2*pi*cycle.sc2[t])*yr.sc[t] + 
-                ps1$delta[5,]*sin(2*pi*cycle.sc2[t])*yr.sc[t] 
+                ps1$delta[2,]*yr.sc[t]^2 +
+                ps1$delta[3,]*cos(2*pi*cycle.sc2[t]) + 
+                ps1$delta[4,]*sin(2*pi*cycle.sc2[t]) +
+                ps1$delta[5,]*cos(2*pi*cycle.sc2[t])*yr.sc[t] + 
+                ps1$delta[6,]*sin(2*pi*cycle.sc2[t])*yr.sc[t] 
 
 }
 
@@ -133,25 +133,23 @@ mab2 <- l.ab2 %>%
 
 df.ab2 <- data.frame( year = yr,
                      md = apply(l.ab2, 1, median, na.rm=T) |> exp(),
-                     lci95 = apply(l.ab2, 1, hdi)[1,]  |> exp(),
-                     uci95 = apply(l.ab2, 1, hdi)[2,] |> exp(),
-                     lci80 = apply(l.ab2, 1, hdi, credMass=.8)[1,] |> exp(),
-                     uci80 = apply(l.ab2, 1, hdi, credMass=.8)[2,] |> exp()
+                     lci95 = apply(l.ab2, 1, HDInterval::hdi)[1,]  |> exp(),
+                     uci95 = apply(l.ab2, 1, HDInterval::hdi)[2,] |> exp(),
+                     lci80 = apply(l.ab2, 1, HDInterval::hdi, credMass=.8)[1,] |> exp(),
+                     uci80 = apply(l.ab2, 1, HDInterval::hdi, credMass=.8)[2,] |> exp()
 )
 # calculate a four year moving average
-for (t in 1:nrow(l.ab2)){
+for (t in 17:nrow(l.ab2)){
 for (i in 1:ncol(l.ab2)){
-ind1 <- ifelse( (t-16)<1, 1, t-16)
-ind2 <- ifelse( (t+16)>nrow(l.ab2), nrow(l.ab2), t+16)
-l.ab3[t,i] <- mean(l.ab2[ind1:ind2, i])
+l.ab3[t,i] <- mean(l.ab2[(t-16):t, i])
 }}
 
 df.ab3 <- data.frame( year = yr,
                       md = apply(l.ab3, 1, median, na.rm=T) |> exp(),
-                      lci95 = apply(l.ab3, 1, hdi)[1,]  |> exp(),
-                      uci95 = apply(l.ab3, 1, hdi)[2,] |> exp(),
-                      lci80 = apply(l.ab3, 1, hdi, credMass=.8)[1,] |> exp(),
-                      uci80 = apply(l.ab3, 1, hdi, credMass=.8)[2,] |> exp()
+                      lci95 = apply(l.ab3, 1, HDInterval::hdi)[1,]  |> exp(),
+                      uci95 = apply(l.ab3, 1, HDInterval::hdi)[2,] |> exp(),
+                      lci80 = apply(l.ab3, 1, HDInterval::hdi, credMass=.8)[1,] |> exp(),
+                      uci80 = apply(l.ab3, 1, HDInterval::hdi, credMass=.8)[2,] |> exp()
 )
 
 df.dat <- data.frame(count=d1$datl$y/d1$constl$effort,
@@ -173,6 +171,7 @@ p2 <- ggplot() + theme_minimal() +
   geom_line(data=df.ab2, aes(x=year, y=uci80), color=col, linewidth=0.5 ) +
   geom_line(data=df.ab2, aes(x=year, y=md), color=col, linewidth=1 ) +
   geom_line(data=df.dat, aes(x=year, y=count), color="blue", linewidth=1, alpha=0.75) +
+  #geom_line(data=df.ab3, aes(x=year, y=md), color="black", linewidth=1, linetype=2) +
   coord_cartesian(xlim=c(1994, 2023), ylim=c(0, 350)) +
   ggtitle("Tadoussac Bird Observatory")
 #+  scale_color_manual(values = cols)
@@ -187,10 +186,12 @@ p2 <- ggplot() + theme_minimal() +
 # summaries
 # which year had max median catches
 df.ab2$year[which.max(df.ab2$md)]
+df.ab2[df.ab2$year>=2004 & df.ab2$year<=2005,]
+df.ab2[df.ab2$year>=2022 & df.ab2$year<=2023,]
 
 #################
 # Plot captures per mist nest hours 
-# for MC site
+# for Whitefish Point site
 range(d2$dat$year)
 yr <- seq(1994, 2023, by=0.5)
 cycle.sc2 <- 1:length(yr)/8
@@ -218,10 +219,10 @@ mab2 <- l.ab2 %>%
 
 df.ab2 <- data.frame( year = yr,
                       md = apply(l.ab2, 1, median, na.rm=T) |> exp(),
-                      lci95 = apply(l.ab2, 1, hdi)[1,]  |> exp(),
-                      uci95 = apply(l.ab2, 1, hdi)[2,] |> exp(),
-                      lci80 = apply(l.ab2, 1, hdi, credMass=.8)[1,] |> exp(),
-                      uci80 = apply(l.ab2, 1, hdi, credMass=.8)[2,] |> exp()
+                      lci95 = apply(l.ab2, 1, HDInterval::hdi)[1,]  |> exp(),
+                      uci95 = apply(l.ab2, 1, HDInterval::hdi)[2,] |> exp(),
+                      lci80 = apply(l.ab2, 1, HDInterval::hdi, credMass=.8)[1,] |> exp(),
+                      uci80 = apply(l.ab2, 1, HDInterval::hdi, credMass=.8)[2,] |> exp()
 )
 # calculate a four year moving average
 for (t in 1:nrow(l.ab2)){
@@ -233,10 +234,10 @@ for (t in 1:nrow(l.ab2)){
 
 df.ab3 <- data.frame( year = yr,
                       md = apply(l.ab3, 1, median, na.rm=T) |> exp(),
-                      lci95 = apply(l.ab3, 1, hdi)[1,]  |> exp(),
-                      uci95 = apply(l.ab3, 1, hdi)[2,] |> exp(),
-                      lci80 = apply(l.ab3, 1, hdi, credMass=.8)[1,] |> exp(),
-                      uci80 = apply(l.ab3, 1, hdi, credMass=.8)[2,] |> exp()
+                      lci95 = apply(l.ab3, 1, HDInterval::hdi)[1,]  |> exp(),
+                      uci95 = apply(l.ab3, 1, HDInterval::hdi)[2,] |> exp(),
+                      lci80 = apply(l.ab3, 1, HDInterval::hdi, credMass=.8)[1,] |> exp(),
+                      uci80 = apply(l.ab3, 1, HDInterval::hdi, credMass=.8)[2,] |> exp()
 )
 
 df.dat <- data.frame(count=d2$datl$y/d2$constl$effort,
@@ -259,7 +260,7 @@ p3 <- ggplot() + theme_minimal() +
   geom_line(data=df.ab2, aes(x=year, y=md), color=col, linewidth=1 ) +
   geom_line(data=df.dat, aes(x=year, y=count), color="blue", linewidth=1, alpha=0.75) +
   coord_cartesian(xlim=c(1994, 2023), ylim=c(0, 150)) +
-  ggtitle("Whitefish Point")
+  ggtitle("Whitefish Point") +
 #+  scale_color_manual(values = cols)
 
 ap23 <- align_plots(p2, p3, align="v", axis="l")
@@ -279,10 +280,10 @@ df.ab2$year[which.max(df.ab2$md)]
 #**********************
 #* Plot population growth over time
 #**********************
-plam <- array(NA, dim=c(constl$ntime-1, 4000), 
+plam <- array(NA, dim=c(d1$constl$ntime-1, 4000), 
               dimnames=list(year=c(1997:2023), iter=c(1:4000)))
 lst2 <- ifelse(lst$lambda.star==0, 0.1, lst$lambda.star )
-for (t in 1:(constl$ntime-1)){
+for (t in 1:(d1$constl$ntime-1)){
   plam[t,] <- lst$lambda.star[t+1,]/lst2[t,]
 }
 
